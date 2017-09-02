dbb381fa8d40c3451668a9260a365ea6bfc6a8ad
K4ugummi <stephan.schauerte@gmail.com>
2017-09-02 21:22:07 +0200

    Add fps display

--- Assets/Standard Assets/Utility/FPSCounter.cs
+++ Assets/Standard Assets/Utility/FPSCounter.cs
@@ -11,14 +11,13 @@ namespace UnityStandardAssets.Utility
         private int m_FpsAccumulator = 0;
         private float m_FpsNextPeriod = 0;
         private int m_CurrentFps;
-        const string display = "{0} FPS";
+        [SerializeField]
         private Text m_Text;
 
 
         private void Start()
         {
             m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
-            m_Text = GetComponent<Text>();
         }
 
 
@@ -31,8 +30,40 @@ namespace UnityStandardAssets.Utility
                 m_CurrentFps = (int) (m_FpsAccumulator/fpsMeasurePeriod);
                 m_FpsAccumulator = 0;
                 m_FpsNextPeriod += fpsMeasurePeriod;
-                m_Text.text = string.Format(display, m_CurrentFps);
+                m_Text.text = "FPS: " + GetFPSColored(m_CurrentFps);
             }
+        }
+
+        string GetFPSColored(int _fps) {
+            string _result = "<color=#";
+            if (_fps < 8) {
+                _result += "B40404";
+            }
+            else if (_fps < 16) {
+                _result += "B43104"; 
+            }
+            else if (_fps < 24) {
+                _result += "B45F04";
+            }
+            else if (_fps < 32) {
+                _result += "B18904"; 
+            }
+            else if (_fps < 40) {
+                _result += "AEB404"; 
+            }
+            else if (_fps < 48) {
+                _result += "86B404";
+            }
+            else if (_fps < 56) {
+                _result += "5FB404"; 
+            }
+            else if (_fps < 62) {
+                _result += "31B404";
+            }
+            else {
+                _result += "04B404";
+            }
+            return _result + ">" + _fps.ToString() + "</color>";
         }
     }
 }
