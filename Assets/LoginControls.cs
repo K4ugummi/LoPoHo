07682a785e357643613838a24aca3f7c19da4953
K4ugummi <stephan.schauerte@gmail.com>
2017-09-02 15:06:49 +0200

    Add basic Login controls

---
+++ Assets/LoginControls.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginControls : MonoBehaviour {
+
+    [SerializeField]
+    LoginMenu loginMenu;
+
+    [SerializeField]
+    InputField loginName;
+    [SerializeField]
+    InputField loginPassword;
+
+    int selected;
+
+	void Start () {
+        loginName.Select();
+        selected = 0;
+	}
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            switch (selected) {
+                case 0:
+                    loginPassword.Select();
+                    selected = 1;
+                    break;
+                case 1:
+                    loginName.Select();
+                    selected = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            loginMenu.Login_LoginButtonPressed();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            loginMenu.Login_ExitButtonPressed();
+        }
+    }
+
+}
---
+++ Assets/RegisterControls.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterControls : MonoBehaviour {
+
+    [SerializeField]
+    LoginMenu loginMenu;
+
+    [SerializeField]
+    InputField registerName;
+    [SerializeField]
+    InputField registerPassword;
+    [SerializeField]
+    InputField registerConfirmPassword;
+
+    int selected;
+
+    void Start() {
+        registerName.Select();
+        selected = 0;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            switch (selected) {
+                case 0:
+                    registerPassword.Select();
+                    selected = 1;
+                    break;
+                case 1:
+                    registerConfirmPassword.Select();
+                    selected = 2;
+                    break;
+                case 2:
+                    registerName.Select();
+                    selected = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            loginMenu.Register_RegisterButtonPressed();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            loginMenu.Register_BackButtonPressed();
+        }
+    }
+}
