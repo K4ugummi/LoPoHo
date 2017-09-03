19067a57a3b8ba33582c6fb70ca51c44cc1623d8
K4ugummi <stephan.schauerte@gmail.com>
2017-09-04 01:22:44 +0200

    Add intro video

---
+++ Assets/Scripts/IntroVideo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IntroVideo : MonoBehaviour {
+
+    private void Start() {
+        StartCoroutine(WaitAndLoad(7.5f, "LoginMenu"));
+    }
+
+    private IEnumerator WaitAndLoad(float value, string scene) {
+        yield return new WaitForSeconds(value);
+        SceneManager.LoadScene(scene);
+    }
+}
